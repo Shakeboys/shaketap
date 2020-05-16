ed0eba39556084ba4549117ec3e21164c56c7f51
shakeboy <576244779@qq.com>
2020-05-16 19:00:02 +0800

    boo

---
+++ JS/fullScreen.js
@@ -0,0 +1,14 @@
+
+/*全屏函数*/
+
+function requestFullScreen(element) {
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+    if (requestMethod) {
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") {
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
